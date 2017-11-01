c8167eae106510300dd18954713ba9f50dd2ebc0
follower <follower@rancidbacon.com>
2017-11-01 23:02:15 +1300

    Add skeleton app which can be successfully compiled.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+// Configure with: CMAKE_PREFIX_PATH="$(brew --prefix qt55)" cmake .
+// Compile with: make
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[]) {
+
+  qDebug("vster");
+
+  QCoreApplication app(argc, argv);
+
+  return app.exec();
+}
